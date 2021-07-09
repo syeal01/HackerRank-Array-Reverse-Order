dc48c52eeea2d197586b153479f248e01e848153
syeal01 <syeal01@hotmail.com>
2021-07-09 11:59:14 -0400

    Initial Checkin

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        arr.Reverse();
+        foreach (var item in arr)
+            System.Console.Write($"{item} ");
+    }
+}
